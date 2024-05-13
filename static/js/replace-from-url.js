6165f52f4e5691042ef025ba35d2fdc8992a067a
Robert Fekete <feketer@Roberts-MacBook-Pro.local>
2024-05-13 15:31:57 +0200

    POC js to replace placeholder IP address from URL parameter

---
+++ static/js/replace-from-url.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Identify the text to be replaced
+    console.log("Hello world!"); 
+    const textToReplace = '%placeholder-ip%';
+    console.log(textToReplace); 
+
+    // Get the URL parameter value
+    const urlParams = new URLSearchParams(window.location.search);
+
+    // FIXME loop through URL parameters beginning with replace-, and replace the relevant placeholder
+    const replaceValue = urlParams.get('replace-ip');
+    console.log(textToReplace, replaceValue); 
+    // If the 'replace-ip' parameter exists in the URL, replace the text
+    // FIXME: if it doesn't exist, replace with a default, or optionally with one from the frontmatter
+    if (replaceValue) { 
+        const contentDiv = document.getElementsByClassName('td-content');
+        const replacedText = textToReplace.replace(textToReplace, replaceValue);
+        console.log(replacedText, textToReplace, replaceValue); 
+
+        replaceRecursively(document.body, new RegExp(textToReplace, "g"), replaceValue);
+
+    }
+});
+
+function replaceRecursively(element, from, to) {
+    if (element.childNodes.length) {
+        element.childNodes.forEach(child => replaceRecursively(child, from, to));
+    } else {
+        const cont = element.textContent;
+        if (cont) element.textContent = cont.replace(from, to);
+    }
+};
+
