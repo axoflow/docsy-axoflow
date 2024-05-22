bac58f0fedcc8b6446230f1a862fe91188ac4048
Robert Fekete <feketer@Roberts-MBP.localdomain>
2024-05-22 15:14:43 +0200

    Make it possible to replace %axorouter-port% from url parameter

--- static/js/replace-from-url.js
+++ static/js/replace-from-url.js
@@ -1,16 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Identify the text to be replaced
-    const textToReplace = '%placeholder-ip%';
 
     // Get the URL parameter value
     const urlParams = new URLSearchParams(window.location.search);
 
     // FIXME loop through URL parameters beginning with replace-, and replace the relevant placeholder
-    const replaceValue = urlParams.get('replace-ip');
-    // console.log(textToReplace, replaceValue); 
-    // If the 'replace-ip' parameter exists in the URL, replace the text
-    // FIXME: if it doesn't exist, replace with a default, or optionally with one from the frontmatter
-    if (replaceValue) { 
+    if (urlParams.has('replace-ip')){
+        console.log("Replacing placeholder IP"); // true
+        const replaceValue = urlParams.get('replace-ip');
+        // Identify the text to be replaced
+        const textToReplace = '%axorouter-ip%';
+        // console.log(textToReplace, replaceValue);
+        // If the 'replace-ip' parameter exists in the URL, replace the text
+        // FIXME: if it doesn't exist, replace with a default, or optionally with one from the frontmatter
+        const contentDiv = document.getElementsByClassName('td-content');
+        const replacedText = textToReplace.replace(textToReplace, replaceValue);
+
+        replaceRecursively(document.body, new RegExp(textToReplace, "g"), replaceValue);
+
+    }
+    if (urlParams.has('replace-port')){
+        console.log("Replacing placeholder port"); // true
+        const replaceValue = urlParams.get('replace-port');
+        // Identify the text to be replaced
+        const textToReplace = '%axorouter-port%';
+        // console.log(textToReplace, replaceValue);
+        // If the 'replace-port' parameter exists in the URL, replace the text
+        // FIXME: if it doesn't exist, replace with a default, or optionally with one from the frontmatter
         const contentDiv = document.getElementsByClassName('td-content');
         const replacedText = textToReplace.replace(textToReplace, replaceValue);
 
